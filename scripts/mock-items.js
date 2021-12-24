317c3f37df642fc06b8b3b3f7ff2118ca9bac302
fvameric2 <fvameric2@gmail.com>
2021-12-25 00:04:14 +0100

    First commit

--- scripts/mock-items.js
+++ scripts/mock-items.js
@@ -1,41 +1,46 @@
 // del 0 al 40
 export let itemNormal = [
-    { nombre: 'planta0', descripcion: 'planta normal', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta1', descripcion: 'planta normal', cantidad: 0, image: 'assets/items/item1.png'},
-    { nombre: 'planta2', descripcion: 'planta normal', cantidad: 0, image: 'assets/items/item0.png'},
+    { nombre: 'Caca', descripcion: 'Huele muy mal', cantidad: 0, image: 'assets/items/item0.png'},
+    { nombre: 'Planta pocha', descripcion: 'planta normal', cantidad: 0, image: 'assets/items/item1.png'},
+    { nombre: 'Planta con potencial', descripcion: 'planta normal', cantidad: 0, image: 'assets/items/item2.png'},
 ]
 
 // del 40 al 70
 export let itemBuena = [
-    { nombre: 'planta3', descripcion: 'planta buena', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta4', descripcion: 'planta buena', cantidad: 0, image: 'assets/items/item4.png'},
-    { nombre: 'planta5', descripcion: 'planta buena', cantidad: 0, image: 'assets/items/item0.png'},
+    { nombre: 'Florecilla', descripcion: 'La típica', cantidad: 0, image: 'assets/items/item3.png'},
+    { nombre: 'Cactus normal', descripcion: 'Es MUY normal', cantidad: 0, image: 'assets/items/item4.png'},
+    { nombre: 'Chorizo', descripcion: 'Bien picante', cantidad: 0, image: 'assets/items/item5.png'},
+    { nombre: 'Bocadillo', descripcion: 'Viene del suelo pero se limpia y ya vale', cantidad: 0, image: 'assets/items/item6.png'},
+    { nombre: 'Chocolate', descripcion: 'Para aliviar las penas', cantidad: 0, image: 'assets/items/item7.png'},
 ]
 
 // del 70 al 80
 export let itemMuyBuena = [
-    { nombre: 'planta6', descripcion: 'planta muy buena', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta7', descripcion: 'planta muy buena', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta8', descripcion: 'planta muy buena', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta9', descripcion: 'planta muy buena', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta10', descripcion: 'planta muy buena', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta11', descripcion: 'planta muy buena', cantidad: 0, image: 'assets/items/item0.png'}
+    { nombre: 'Pintauñas negro', descripcion: 'Gotica culona', cantidad: 0, image: 'assets/items/item8.png'},
+    { nombre: 'Dorayaki', descripcion: 'Ojalá Doraemon fuera real', cantidad: 0, image: 'assets/items/item9.png'},
+    { nombre: 'Haworthia', descripcion: 'Peligrosamente puntiaguda', cantidad: 0, image: 'assets/items/item10.png'},
+    { nombre: 'Kofun', descripcion: 'Tumba antigua japonesa, toda entera para ti', cantidad: 0, image: 'assets/items/item11.png'}
 ]
 
 // del 80 al 90
 export let itemMuuuyBuena = [
-    { nombre: 'planta12', descripcion: 'planta muuuy buena', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta13', descripcion: 'planta muuuy buena', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta14', descripcion: 'planta muuuy buena', cantidad: 0, image: 'assets/items/item0.png'}
+    { nombre: 'Galletas de shin chan', descripcion: 'Come on baby, come on baby', cantidad: 0, image: 'assets/items/item12.png'},
+    { nombre: 'Niños del Rhythm Heaven', descripcion: 'Coro y Rhythm Heaven, 2x1', cantidad: 0, image: 'assets/items/item13.png'},
+    { nombre: 'Patapon', descripcion: 'El mejor juego de la historia', cantidad: 0, image: 'assets/items/item14.png'},
+    { nombre: 'Vivi', descripcion: 'No podía faltar algo del FFIX', cantidad: 0, image: 'assets/items/item15.png'},
+    { nombre: 'Merluza con gambas y jamón', descripcion: 'Que no se me olvide la mayonesa', cantidad: 0, image: 'assets/items/item16.png'}
 ]
 
 // del 90 al 95
 export let itemGenial = [
-    { nombre: 'planta15', descripcion: 'planta genial', cantidad: 0, image: 'assets/items/item0.png'},
-    { nombre: 'planta16', descripcion: 'planta genial', cantidad: 0, image: 'assets/items/item0.png'}
+    { nombre: 'Martillo de Old Boy', descripcion: 'Lula por dios, eso es peligrosísimo', cantidad: 0, image: 'assets/items/item17.png'},
+    { nombre: 'RTX 3070', descripcion: 'Es imposible de creer pero ahí estaba, tirada en el suelo', cantidad: 0, image: 'assets/items/item18.png'},
+    { nombre: 'Suculenta', descripcion: 'Y encima está perfecta, a cuidarla bien!', cantidad: 0, image: 'assets/items/item19.png'},
+    { nombre: 'Camera Obscura', descripcion: 'Algo malo va a pasar si la tienes!', cantidad: 0, image: 'assets/items/item20.png'},
 ]
 
 // del 95 al 100
 export let itemPerfecta = [
-    { nombre: 'planta17', descripcion: 'planta perfecta', cantidad: 0, image: 'assets/items/item0.png'}
+    { nombre: 'Totoro de peluche', descripcion: 'aruko aruko watashi wa genki!', cantidad: 0, image: 'assets/items/item21.png'},
+    { nombre: 'Pingu', descripcion: 'El mismísimo, viene a saludarte por navidad!', cantidad: 0, image: 'assets/items/item22.png'}
 ]
